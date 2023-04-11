af297b99db06521e1f7c7e369b93cf482d45ad41
Luis Esteban Rodriguez <rodriguezjluis0@gmail.com>
2023-04-11 14:09:13 +0200

    refactor: update webview

--- cloud_reader.go
+++ cloud_reader.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/zserge/webview"
+import "github.com/webview/webview"
 
 func main() {
-	webview.Open("Amazon Kindle",
-		"https://read.amazon.com/",  800, 600, true)
+	w := webview.New(true)
+	defer w.Destroy()
+	w.SetTitle("Amazon Kindle Reader")
+	w.SetSize(800, 600, webview.HintNone)
+	w.Navigate("https://read.amazon.com/")
+	w.Run()
 }
